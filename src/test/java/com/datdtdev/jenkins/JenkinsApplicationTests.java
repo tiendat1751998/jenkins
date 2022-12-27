e70fb5d7c74b0ad13172d3acaa89de45a6bb7a57
dotie <dotiendat1751998@gmail.com>
2022-12-27 09:12:02 +0700

    Initial commit

---
+++ src/test/java/com/datdtdev/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datdtdev.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
